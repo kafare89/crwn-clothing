53056340bc60164045047584fbb47890c12f7446
Katheryne Fallas <katheryne.fallas@avantica.net>
2021-02-13 17:38:22 -0600

    finishing sign in, handle submit method

--- src/App.js
+++ src/App.js
@@ -25,14 +25,12 @@ class App extends React.Component {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          console.log(snapShot.data());
           this.setState({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data()
             }
           });
-          console.log(this.state);
         });
       } else {
         this.setState({
